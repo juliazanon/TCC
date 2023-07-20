0cfb2c14eabf43617b45ec7d878bf5d7b74f8cfb
Julia <julia_zanon@usp.br>
2023-07-20 08:07:41 +0200

    first circle

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using SharpGL;
+using SharpGL.Enumerations;
+using SharpGL.SceneGraph;
+using GlmNet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +16,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using SharpGL.WPF;
 
 namespace TCC
 {
@@ -23,6 +33,66 @@ namespace TCC
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //  Camera parameters
+        float[] _viewPoint = new float[] { 0.0f, 0.0f, 0.0f };
+        float[] _position = new float[] { 0.0f, 0.0f, 10.0f };
+        float[] _upVector = new float[] { 0.0f, 1.0f, 0.0f };
+        //float _moveDistance = 1.0f;
+        float scale = 0.15f;
+
+        private void OpenGLDraw(object sender, SharpGL.WPF.OpenGLRoutedEventArgs args)
+        {
+            OpenGL gl = GLControl.OpenGL;
+            float w = (float)GLControl.ActualWidth;
+            float h = (float)GLControl.ActualHeight;
+
+            //  Perspective projection
+            gl.MatrixMode(MatrixMode.Projection);
+            gl.LoadIdentity();
+            gl.Perspective(60, (int)w / h, 0.01, 1000);
+
+            //  Clear The Screen And The Depth Buffer
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            gl.Scale(scale, scale, 1);
+
+            //  Camera Position
+            gl.LookAt(_position[0], _position[1], _position[2],
+                _viewPoint[0], _viewPoint[1], _viewPoint[2],
+                _upVector[0], _upVector[1], _upVector[2]);
+
+            gl.MatrixMode(MatrixMode.Modelview);
+
+            // Background color
+            vec3 backrgb = new vec3(225, 227, 226) / 255;
+            gl.ClearColor(backrgb.x, backrgb.y, backrgb.z, 0);
+
+
+            vec3 rgb = new vec3(111, 112, 112) / 255;
+            CreateCircle(1000, 30, rgb);
+        }
+
+        private void CreateCircle(int n, float r, vec3 rgb)
+        {
+            OpenGL gl = GLControl.OpenGL;
+            gl.Begin(BeginMode.Lines);
+            gl.Color(rgb.x, rgb.y, rgb.z, 0);
+
+            double x, y;
+
+            for (int i = 0; i < n; i++)
+            {
+                x = r * Math.Cos(i);
+                y = r * Math.Sin(i);
+                gl.Vertex(x, y, 0);
+
+                x = r * Math.Cos(i + 0.1);
+                y = r * Math.Sin(i + 0.1);
+                gl.Vertex(x, y, 0);
+            }
+            gl.End();
         }
     }
 }
